26d6a5069eed1e8d8f08d92707872efb2afe6b24
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2017-11-04 10:38:11 +1100

    Adding the initial structure of the project

---
+++ src/main/java/com/kds/boot/recip/RecipApplication.java
@@ -0,0 +1,12 @@
+package com.kds.boot.recip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kds/boot/recip/RecipApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kds.boot.recip;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
