dca8332c8a3bf152ad276f6d1a2c8388e92ecf5b
Kalpa Senanayak <senanayake.kalpa@gmail.com>
2017-11-12 12:26:48 +1100

    Resolving the circuler dependency issue when using jpa and lombok

--- src/main/java/com/kds/boot/recip/model/Ingredient.java
+++ src/main/java/com/kds/boot/recip/model/Ingredient.java
@@ -5,8 +5,8 @@ import lombok.*;
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-@Getter
-@Setter
+@Data
+@EqualsAndHashCode(exclude = {"recipes"})
 @Entity
 @NoArgsConstructor
 public class Ingredient {
--- src/main/java/com/kds/boot/recip/model/Notes.java
+++ src/main/java/com/kds/boot/recip/model/Notes.java
@@ -1,12 +1,14 @@
 package com.kds.boot.recip.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 
-@Getter
-@Setter
+@Data
+@EqualsAndHashCode(exclude = {"recipes"})
 @Entity
 public class Notes {
 
